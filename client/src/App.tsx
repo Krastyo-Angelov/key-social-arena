0ca3ef0e556dd11792fb802fd0d5bd5b3ab6bd82
“blubito-krastyo-angelov-01” <krastyo.3vc9.angelov@misumi.co.jp>
2024-04-20 12:23:48 +0300

    linter set up

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,25 @@
-import "./App.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Home } from "./pages/home/Home";
-import { Register } from "./pages/register/Register";
-import { PathConstants } from "./constants/path-constants";
-import { Layout } from "./components/layout/Layout";
-import { Error } from "./pages/error/Error";
+import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Home } from './pages/home/Home';
+import { Register } from './pages/register/Register';
+import { PathConstants } from './constants/path-constants';
+import { Layout } from './components/layout/Layout';
+import { Error } from './pages/error/Error';
 
 export default function App() {
-  const router = createBrowserRouter([
-    {
-      // parent route component
-      element: <Layout />,
-      // error route component
-      errorElement: <Error />,
-      // children route components
-      children: [
-        { path: PathConstants.HOME, element: <Home /> },
-        { path: PathConstants.REGISTER, element: <Register /> },
-      ],
-    },
-  ]);
+	const router = createBrowserRouter([
+		{
+			// parent route component
+			element: <Layout />,
+			// error route component
+			errorElement: <Error />,
+			// children route components
+			children: [
+				{ path: PathConstants.HOME, element: <Home /> },
+				{ path: PathConstants.REGISTER, element: <Register /> },
+			],
+		},
+	]);
 
-  return <RouterProvider router={router} />;
+	return <RouterProvider router={router} />;
 }
