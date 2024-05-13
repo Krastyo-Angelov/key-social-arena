be3d0c2afc420e71b15772926c8bf6f7d356ed42
“blubito-krastyo-angelov-01” <krastyo.3vc9.angelov@misumi.co.jp>
2024-05-13 11:28:50 +0300

    refactor

--- client/src/components/layout/Layout.tsx
+++ client/src/components/layout/Layout.tsx
@@ -1,15 +1,15 @@
-import { Outlet } from "react-router-dom";
-import { Navbar } from "../navbar/Navbar";
-import { Footer } from "../footer/Footer";
+import { Outlet } from 'react-router-dom';
+import { Header } from '../header/Header';
+import { Footer } from '../footer/Footer';
 
 export function Layout() {
-  return (
-    <>
-      <Navbar />
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<main>
+				<Outlet />
+			</main>
+			<Footer />
+		</>
+	);
 }
