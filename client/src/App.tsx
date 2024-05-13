c99d9f1210b44d5e6d38ce3007f221957ea2961e
“blubito-krastyo-angelov-01” <krastyo.3vc9.angelov@misumi.co.jp>
2024-05-13 14:16:05 +0300

    refactored to display the Register Page when there is no user logged in

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,45 @@
 import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+	createBrowserRouter,
+	Navigate,
+	RouterProvider,
+} from 'react-router-dom';
 import { Home } from './pages/home/Home';
 import { Register } from './pages/register/Register';
 import { PathConstants } from './constants/path-constants';
 import { Layout } from './components/layout/Layout';
 import { Error } from './pages/error/Error';
+import { ReactNode, useState } from 'react';
 
 export default function App() {
+	const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+	const ProtectedRoute = ({ children }: { children: ReactNode }) => {
+		// If there user is not logged in
+		if (!isLoggedIn) {
+			return <Navigate to={PathConstants.REGISTER} />;
+		}
+
+		return <>{children}</>;
+	};
+
 	const router = createBrowserRouter([
 		{
+			path: '/',
 			// parent route component
-			element: <Layout />,
+			element: (
+				<ProtectedRoute>
+					<Layout />
+				</ProtectedRoute>
+			),
 			// error route component
 			errorElement: <Error />,
 			// children route components
-			children: [
-				{ path: PathConstants.HOME, element: <Home /> },
-				{ path: PathConstants.REGISTER, element: <Register /> },
-			],
+			children: [{ path: PathConstants.HOME, element: <Home /> }],
+		},
+		{
+			path: '/register',
+			element: <Register />,
 		},
 	]);
 
